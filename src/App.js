c09a4e5ab8836be8eaa91cf7dc141e1a8c0e6d38
Zion Abubakar <impulseditss@gmail.com>
2026-03-23 20:57:27 +0100

    more updates and fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { StudyGramProvider } from "./context/StudyGramContext";
 import { CoursePlayerProvider } from "./context/CoursePlayerContext";
