e56034aba24452c5bb76d2b0035e7a54c232655a
Zion Abubakar <impulseditss@gmail.com>
2026-07-01 19:18:51 +0100

    Update AuthModal.jsx

--- src/components/modals/AuthModal.jsx
+++ src/components/modals/AuthModal.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/AuthContext";
 import { useUI } from "../../context/UIContext";
 import { toast } from "sonner";
 import LoadingSpinner from "../common/LoadingSpinner";
-import { supabase } from "../../utils/supabase";
+
 
 const AuthModal = () => {
   const { showAuthModal, setShowAuthModal, pendingAction, setPendingAction, setShowUpgradeModal } = useUI();
